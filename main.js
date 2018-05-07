b51dda929ab2b1e32ae6fe1d7d6c4d90fe2103a3
Alexandra Govorkova <al.govorkova@epam.com>
2018-05-07 14:37:41 +0300

    Add test for transparent window

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
 'use strict';
 
-const { app, BrowserWindow, globalShortcut } = require('electron');
-const { systemPreferences } = require('electron');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
 let win;
 
 app.on('ready', () => {
-    win = new BrowserWindow({ width: 1000, height: 800 });
+    win = new BrowserWindow({
+        width: 200,
+        height: 40,
+        show: false,
+        resizable: false,
+        x: 300,
+        y: 300,
+        alwaysOnTop: true,
+        frame: false,
+        transparent: true
+    });
 
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html')
     }));
 
+    win.on('ready-to-show', () => {
+        console.log('ready event');
+        win.show();
+    });
+
     win.on('closed', () => {
         win = null
     });
-
-    // Register a 'CommandOrControl+X' shortcut listener.
-    const ret = globalShortcut.register('CommandOrControl+X', () => {
-        console.log('CommandOrControl+X is pressed');
-    });
-
-    if (!ret) {
-        console.log('registration failed');
-    }
-
-    // Check whether a shortcut is registered.
-    console.log(globalShortcut.isRegistered('CommandOrControl+X'));
-});
-
-app.on('will-quit', () => {
-    globalShortcut.unregisterAll();
 });
