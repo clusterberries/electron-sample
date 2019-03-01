9860bbfa1713157c99f7bf81be1210030f57ed67
Alexandra Govorkova <al.govorkova@epam.com>
2019-03-01 13:03:09 +0300

    Display available screens

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 'use strict';
 
-const { app, BrowserWindow, globalShortcut } = require('electron');
-const { systemPreferences } = require('electron');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
 let win;
 
 app.on('ready', () => {
-    win = new BrowserWindow({ width: 1000, height: 800 });
+    win = new BrowserWindow({
+        width: 600,
+        height: 600,
+        show: true,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
 
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html')
@@ -16,20 +22,4 @@ app.on('ready', () => {
     win.on('closed', () => {
         win = null
     });
-
-    // Register a 'CommandOrControl+X' shortcut listener.
-    const ret = globalShortcut.register('CommandOrControl+X', () => {
-        console.log('CommandOrControl+X is pressed');
-    });
-
-    if (!ret) {
-        console.log('registration failed');
-    }
-
-    // Check whether a shortcut is registered.
-    console.log(globalShortcut.isRegistered('CommandOrControl+X'));
-});
-
-app.on('will-quit', () => {
-    globalShortcut.unregisterAll();
 });
