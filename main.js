76d84e85fd0dde1835d96958610d975d759ea404
Alexandra Govorkova <al.govorkova@epam.com>
2019-05-30 14:57:29 +0300

    Set up case to reproduce flash

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 'use strict';
 
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const url = require('url');
 let win;
+let childWin;
 
 app.on('ready', () => {
+    // console.log(process.versions);
+
     win = new BrowserWindow({
         width: 600,
         height: 600,
@@ -25,5 +28,21 @@ app.on('ready', () => {
 
     win.setMenu(null);
 
-    console.log(process.versions);
+
+    childWin = new BrowserWindow({
+        width: 500,
+        height: 400,
+        show: true
+    });
+
+    childWin.loadURL(url.format({
+        pathname: path.join(__dirname, 'child.html')
+    }));
+
+    childWin.setMenu(null);
+});
+
+ipcMain.on('flash', (event, isBlinking) => {
+    console.log('flash frame:', isBlinking);
+    childWin.flashFrame(isBlinking);
 });
--- renderer.js
+++ renderer.js
@@ -1,31 +1,12 @@
 console.log('script!');
 
 var electron = require('electron');
-var desktopCapturer = electron.desktopCapturer;
+var ipc = electron.ipcRenderer;
 
-var container = document.getElementById('screens-container');
+var button = document.getElementById('flash');
+var isBlinking = false;
 
-desktopCapturer.getSources({ types: ['screen'] }, function (error, sources) {
-    if (error) {
-        console.log('ERROR: Unable to get sources.', error);
-        return;
-    }
-
-    console.log('Received ' + sources.length + ' sources.', sources);
-
-    var innerHTML = '';
-
-    sources.forEach(source => {
-        source.imgSrc = source.thumbnail.toDataURL();
-
-        innerHTML += `
-<li>
-    <div class="source-img">
-        <img src="${source.imgSrc}" alt="">
-    </div>
-    <div class="text">${source.name}</div>
-</li>`
-    });
-
-    container.innerHTML = innerHTML;
+button.addEventListener('click', () => {
+    isBlinking = !isBlinking;
+    ipc.send('flash', isBlinking);
 });
